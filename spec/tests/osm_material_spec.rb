fac6c806967c964353c9d9f4e88446a068b483d4
tanushree04 <47833030+tanushree04@users.noreply.github.com>
2021-04-01 13:43:29 -0600

    feat(material): Add reverse translators for materials
    * feat(material): Add reverse translator for materials
    
    * fix(material): add spaces at the end of files
    
    * feat(material): Add reverse translator for materials
    
    * fix(material): add spaces at the end of files
    
    * fix(material): remove identifier, user data for materials
    
    * fix(material): Add boost optional check, identifier

---
+++ spec/tests/osm_material_spec.rb
@@ -0,0 +1,106 @@
+# *******************************************************************************
+# Honeybee OpenStudio Gem, Copyright (c) 2020, Alliance for Sustainable
+# Energy, LLC, Ladybug Tools LLC and other contributors. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
+# UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF
+# THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+require_relative '../spec_helper'
+
+def load_file(file_name)
+  # point to sample osm file
+  file = File.join(File.dirname(__FILE__), '../samples/osm_material/', file_name)
+  vt = OpenStudio::OSVersion::VersionTranslator.new
+  osm = vt.loadModel(file)
+  return osm
+end
+
+RSpec.describe Honeybee do
+
+  # create output folder for HB JSON materials
+  output_dir = File.join(File.dirname(__FILE__), '../output/osm_material/')
+  FileUtils.mkdir_p(output_dir)
+
+  it 'can load OSM and translate StandardOpaqueMaterial  to Honeybee' do
+
+    openstudio_model = load_file('energyMaterial.osm')
+
+    # create HB JSON material from OS model
+    honeybee = Honeybee::Model.materials_from_model(openstudio_model.get)
+
+    # check values
+    expect(honeybee.size).to eq 1
+    expect(honeybee).not_to be nil
+    expect(honeybee[0][:type]).to eq 'EnergyMaterial'
+    expect(honeybee[0][:identifier]).to eq '1/2IN Gypsum'
+    expect(honeybee[0][:conductivity]).to eq 0.16
+    
+    FileUtils.mkdir_p(output_dir)
+    File.open(File.join(output_dir,'energyMaterial.hbjson'), 'w') do |f|
+      f.puts JSON::pretty_generate(honeybee[0])
+    end
+
+  end
+
+  it 'can load OSM and translate MasslessOpaqueMaterial to Honeybee' do
+
+    openstudio_model = load_file('matNoMass.osm')
+
+    # create HB JSON material from OS model
+    honeybee = Honeybee::Model.materials_from_model(openstudio_model.get)
+
+    # check values
+    expect(honeybee.size).to eq 1
+    expect(honeybee).not_to be nil
+    expect(honeybee[0][:type]).to eq 'EnergyMaterialNoMass'
+    expect(honeybee[0][:identifier]).to eq 'CP02 CARPET PAD'
+    expect(honeybee[0][:roughness]).to eq 'Smooth'
+
+    File.open(File.join(output_dir,'energyMaterialNoMass.hbjson'), 'w') do |f|
+      f.puts JSON::pretty_generate(honeybee[0])
+    end
+  end
+
+  it 'can load OSM and translate SimpleGlazingSystem to Honeybee' do
+    openstudio_model = load_file('simGlazSys.osm')
+
+    # create HB JSON material from OS model
+    honeybee = Honeybee::Model.materials_from_model(openstudio_model.get)
+
+    # check values
+    expect(honeybee.size).to eq 1
+    expect(honeybee).not_to be nil
+    expect(honeybee[0][:type]).to eq 'EnergyWindowMaterialSimpleGlazSys'
+    expect(honeybee[0][:identifier]).to eq 'Simple Glazing'
+    expect(honeybee[0][:shgc]).to eq 0.39
+
+    File.open(File.join(output_dir,'simGlazSys.hbjson'), 'w') do |f|
+      f.puts JSON::pretty_generate(honeybee[0])
+    end
+  end
+
+end
